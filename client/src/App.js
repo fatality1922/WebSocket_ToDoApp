79ed36caac9d5dc3ea9e8abe5c1ace57e6c188fd
Danciak <fatality1922@gmail.com>
2021-05-01 21:52:35 +0200

    problem with server to fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import io from 'socket.io-client';
+import { v4 as uuidv4 } from "uuid";
 
 class App extends React.Component {
 
@@ -10,8 +11,38 @@ class App extends React.Component {
 
   componentDidMount() {
     this.socket = io.connect("localhost:8000");
+    this.socket.on("removeTask", (taskId) => this.removeTask(taskId));
+    this.socket.on("addTask", (task) => this.addTask(task));
+    this.socket.on("updateData", (tasks) => this.updateTasks(tasks));
 
   }
+
+  removeTask(id, isLocal) {
+    const { tasks } = this.state;
+    this.setState({
+      tasks: tasks.filter((task) => task.id !== id)
+    });
+    if (isLocal) {
+      this.socket.emit('removeTask', id);
+    }
+  }
+
+  submitForm = (event) => {
+    event.preventDefault();
+    const task = { name: this.state.taskName, id: uuidv4() };
+    this.addTask(task);
+    this.socket.emit("addTask", task);
+    this.setState({ taskName: "" });
+  };
+
+  addTask = (task) => {
+    this.setState({ tasks: [...this.state.tasks, task] });
+  };
+
+  updateTasks = (tasks) => {
+    this.setState({ tasks});
+  }
+
 
   render() {
     const { tasks, taskName } = this.state;
@@ -33,12 +64,16 @@ class App extends React.Component {
                 <button className='btn btn--red' onClick={() => this.removeTask(task.id, true)}> Remove </button>
               </li>
             ))}
-            
+
           </ul>
 
-          <form id="add-task-form">
-            <input className="text-input" autocomplete="off" type="text" placeholder="Type your description" id="task-name" />
-            <button className="btn" type="submit">Add</button>
+          <form id="add-task-form" onSubmit={(submit) => this.submitForm(submit)}>
+            <input className="text-input" autocomplete="off" type="text" placeholder="Type your description"
+              id="task-name" value={taskName}
+              onChange={(event) =>
+                this.setState({ taskName: event.target.value })
+              } />
+            <button className="btn" type="submit" disabled={!taskName}>Add</button>
           </form>
 
         </section>
